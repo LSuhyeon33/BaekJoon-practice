64711d94711b6b037bc1dc159cdac5a0825f24ca
LSuhyeon33 <gus989533@gmail.com>
2023-11-05 19:00:01 +0900

    단계별로 풀어보기/1단계/입출력과 사칙연산

--- scr/입출력과 사칙연산/2_1000.java
+++ scr/입출력과 사칙연산/2_1000.java
@@ -20,7 +20,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class aAddb {
+public class 2_1000 {
   public static void main(String[] args) throws IOException {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
