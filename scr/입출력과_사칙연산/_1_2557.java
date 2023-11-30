4884cee24b04c9be5fb00b212a8437d35fb849e0
LSuhyeon33 <gus989533@gmail.com>
2023-11-30 17:29:24 +0900

    단계별로 풀어보기/1단계/입출력과 사칙연산

--- scr/입출력과_사칙연산/_1_2557.java
+++ scr/입출력과_사칙연산/_1_2557.java
@@ -2,8 +2,6 @@
 // 문제 이름		Hello World
 // 문제 내용		Hello World!를 화면에 출력하는 문제(예제 출력과 똑같이 출력해야 한다.)
 
-package scr.입출력과_사칙연산;
-
 public class _1_2557 {
   public static void main(String[] args) {
 		System.out.println("Hello World!");
